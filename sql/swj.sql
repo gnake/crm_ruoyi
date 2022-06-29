1b0c4aba39795fe605af9d88ccf17284499af66e
swj <2575901953@qq.com>
2022-06-29 15:58:31 +0800

    提交swj.sql文件 两个模块数据库设计：         1.任务周期模块数据库设计           1.1任务周期定义表         2.我的任务模块数据库设计           2.1我的任务细项表           2.2任务计划&总结表

--- sql/swj.sql
+++ sql/swj.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS MY_TASKS;
 create table IF NOT EXISTS MY_TASKS
 (
     TASK_ID           bigint not null primary key,
-    ORG_ID            bigint comment '潜在客户(ORG_INFO) 关联',
-    CUST_ID           bigint comment '客户管理(CUSTOMER_INFO) 关联',
-    TASK_REVIEW_ID    bigint comment '任务计划&总结(TASK_REVIEW) 关联',
+    ORG_ID            bigint not null comment '潜在客户(ORG_INFO) 关联',
+    CUST_ID           bigint not null comment '客户管理(CUSTOMER_INFO) 关联',
+    TASK_REVIEW_ID    bigint not null comment '任务计划&总结(TASK_REVIEW) 关联',
     SALE_ID           bigint comment '销售人员',
     TASK_FOLLOW_STATE varchar(32) comment '跟进状态',
     TASK_CLASS        varchar(32) comment '任务类别',
@@ -19,7 +19,7 @@ DROP TABLE IF EXISTS TASK_REVIEW;
 CREATE TABLE IF NOT EXISTS TASK_REVIEW
 (
     `TASK_REVIEW_ID`               bigint not null primary key,
-    `TC_ID`                        bigint comment '任务周期定义(TASK_CYCLE) 关联',
+    `TC_ID`                        bigint not null comment '任务周期定义(TASK_CYCLE) 关联',
     `SALE_ID`                      char(36) comment '销售人员',
     `TASK_REVIEW_STATE`            varchar(32) comment '总结状态',
     `TASK_REVIEW_DESC`             text comment '总结说明',
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS TASK_REVIEW
 DROP TABLE IF EXISTS TASK_CYCLE;
 create table IF NOT EXISTS TASK_CYCLE
 (
-    TC_ID    bigint primary key,
+    TC_ID    bigint not null primary key,
     TC_BEGIN varchar(32) null comment '起始日期',
     TC_END   varchar(32) null comment '结束日期'
 ) DEFAULT CHARSET = utf8mb4 comment '任务周期定义表';
