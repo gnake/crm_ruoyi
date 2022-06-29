72b05548b8a2473013db08da302d24e615da6b13
“童楷涵” <2392282052@qq.com>
2022-06-29 14:47:56 +0800

    编码管理和编码类型的数据库，编码管理的编码标识关联编码类型。

---
+++ sql/tkh.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS `SYS_CODETYPE`;
+create table if not exists SYS_CODETYPE
+(
+    TYPE_ID      bigint not null primary key comment '主键',
+    TYPE_IDENT   varchar(32) comment '类型标识',
+    TYPE_NAME    varchar(32) comment '类型名称',
+    TYPE_GROUP   varchar(32) comment '类型分组',
+    IS_EDITABLE  int(1) comment '是否可编辑',
+    IS_EFFECTIVE int(1) comment '是否失效',
+    IS_EXTEND    int(1) comment '是否SQL扩展'
+    ) comment '编码类型'
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4;
+
+DROP TABLE IF EXISTS `SYS_CODELIST`;
+create table if not exists SYS_CODELIST
+(
+    CODE_ID    bigint not null primary key comment '主键',
+    TYPE_ID    bigint comment '编码标识,关联SYS_CODETYPE',
+    CODE_VALUE varchar(36) comment '编码值',
+    CODE_SORT  int(11) comment '编码排序'
+    ) comment '编码管理'
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4;
