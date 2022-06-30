b9f27b2a4f9da1eb91ce5225a402aebafa59b613
swj <2575901953@qq.com>
2022-06-30 10:45:20 +0800

    修改了日期的数据类型

--- sql/CMR_task_swj.sql
+++ sql/CMR_task_swj.sql
@@ -2,15 +2,15 @@
 DROP TABLE IF EXISTS MY_TASKS;
 create table IF NOT EXISTS MY_TASKS
 (
-    TASK_ID           bigint not null primary key,
-    ORG_ID            bigint not null comment '潜在客户(ORG_INFO) 关联',
-    CUST_ID           bigint not null comment '客户管理(CUSTOMER_INFO) 关联',
-    TASK_REVIEW_ID    bigint not null comment '任务计划&总结(TASK_REVIEW) 关联',
+    TASK_ID           bigint primary key,
+    ORG_ID            bigint comment '潜在客户(ORG_INFO) 关联',
+    CUST_ID           bigint comment '客户管理(CUSTOMER_INFO) 关联',
+    TASK_REVIEW_ID    bigint comment '任务计划&总结(TASK_REVIEW) 关联',
     SALE_ID           bigint comment '销售人员',
     TASK_FOLLOW_STATE varchar(32) comment '跟进状态',
     TASK_CLASS        varchar(32) comment '任务类别',
-    TASK_CREATE_TIME  varchar(32) comment '创建任务时间',
-    TASK_FINISH_TIME  varchar(32) comment '任务完成时间',
+    TASK_CREATE_TIME  datatime comment '创建任务时间',
+    TASK_FINISH_TIME  datatime comment '任务完成时间',
     TASK_CUST_STATE   varchar(32) comment '客户状态'
     ) DEFAULT CHARSET = utf8mb4 comment '我的任务细项表';
 
@@ -18,8 +18,8 @@ create table IF NOT EXISTS MY_TASKS
 DROP TABLE IF EXISTS TASK_REVIEW;
 CREATE TABLE IF NOT EXISTS TASK_REVIEW
 (
-    `TASK_REVIEW_ID`               bigint not null primary key,
-    `TC_ID`                        bigint not null comment '任务周期定义(TASK_CYCLE) 关联',
+    `TASK_REVIEW_ID`               bigint primary key,
+    `TC_ID`                        bigint comment '任务周期定义(TASK_CYCLE) 关联',
     `SALE_ID`                      char(36) comment '销售人员',
     `TASK_REVIEW_STATE`            varchar(32) comment '总结状态',
     `TASK_REVIEW_DESC`             text comment '总结说明',
@@ -35,9 +35,9 @@ CREATE TABLE IF NOT EXISTS TASK_REVIEW
 DROP TABLE IF EXISTS TASK_CYCLE;
 create table IF NOT EXISTS TASK_CYCLE
 (
-    TC_ID    bigint not null primary key,
-    TC_BEGIN varchar(32) null comment '起始日期',
-    TC_END   varchar(32) null comment '结束日期'
+    TC_ID    bigint primary key,
+    TC_BEGIN datatime comment '起始日期',
+    TC_END   datatime comment '结束日期'
     ) DEFAULT CHARSET = utf8mb4 comment '任务周期定义表';
 
 #拜访记录
@@ -78,7 +78,7 @@ CREATE TABLE IF NOT EXISTS opp_info (
     `OPP_DES` varchar(256) comment '商机说明',
     `OPP_CONCERN_PRODUCT` varchar(256) comment '关注产品',
     `OPP_EXPECT_INVEST` decimal(10,2) comment '预期投入',
-    `OPP_START_TIME` varchar(32) comment '启动时间',
+    `OPP_START_TIME` datatime comment '启动时间',
     `OPP_STATE` varchar(32) comment '状态',
     `CLUE_SALESMAN` char(36) comment '销售人员',
     `OPP_CREATER` char(36) comment '创建人',
