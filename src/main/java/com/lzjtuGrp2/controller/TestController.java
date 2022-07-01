472624703101fe32bd3a214453bfaa9574635962
庶宏 高 <209597268@qq.com>
2022-07-01 15:26:00 +0800

    搭建一个springboot单体项目

--- src/main/java/com/lzjtuGrp2/controller/TestController.java
+++ src/main/java/com/lzjtuGrp2/controller/TestController.java
@@ -1,7 +1,13 @@
 package com.lzjtuGrp2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 }
