0839240c2e5b72d33851ee09dbc64b1e81803f9b
庶宏 高 <209597268@qq.com>
2022-07-03 12:28:13 +0800

    删除.idea target

---
+++ src/main/java/com/lzjtugrp2/controller/MyCustomerController.java
@@ -0,0 +1,15 @@
+package com.lzjtugrp2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MyCustomerController {
+
+    @GetMapping("/customer")
+    public String index(){
+        return "mycustomer_editable";
+    }
+}
