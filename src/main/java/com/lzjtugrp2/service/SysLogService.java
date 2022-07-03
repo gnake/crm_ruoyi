603e0df0430a20223350e338c8a6d6c66144a0fd
国文 高 <1498107871@qq.com>
2022-07-03 15:48:31 +0800

    提交了sysLogService层

--- src/main/java/com/lzjtugrp2/service/SysLogService.java
+++ src/main/java/com/lzjtugrp2/service/SysLogService.java
@@ -1,4 +1,14 @@
 package com.lzjtugrp2.service;
 
+import com.lzjtugrp2.domain.SysLog;
+
+import java.util.Date;
+import java.util.List;
+
 public interface SysLogService {
+    List<SysLog> examineSysLogs();
+
+    SysLog selectByPrimaryKey(Long id);
+
+    List<SysLog> getSysLogsByMultipleConditions(Date startOperTime, Date endOperTime, String ipAddtress, Long userId, String funcName, String actionType);
 }
--- src/main/java/com/lzjtugrp2/service/impl/SysLogServiceImpl.java
+++ src/main/java/com/lzjtugrp2/service/impl/SysLogServiceImpl.java
@@ -1,10 +1,29 @@
 package com.lzjtugrp2.service.impl;
 
+import com.lzjtugrp2.domain.SysLog;
 import com.lzjtugrp2.mapper.SysLogMapper;
 import com.lzjtugrp2.service.SysLogService;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
+
 @Service
 public class SysLogServiceImpl implements SysLogService {
     private SysLogMapper sysLogMapper;
+
+    @Override
+    public List<SysLog> examineSysLogs() {
+        return sysLogMapper.examineSysLogs();
+    }
+
+    @Override
+    public SysLog selectByPrimaryKey(Long id) {
+        return sysLogMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public List<SysLog> getSysLogsByMultipleConditions(Date startOperTime, Date endOperTime, String ipAddtress, Long userId, String funcName, String actionType) {
+        return sysLogMapper.getSysLogsByMultipleConditions(startOperTime,endOperTime,ipAddtress,userId,funcName,actionType);
+    }
 }
