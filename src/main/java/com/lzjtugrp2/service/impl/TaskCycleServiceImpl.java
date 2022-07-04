a4802c97ed865145b518e6da225cd0d94a23b837
swj <2575901953@qq.com>
2022-07-04 14:34:06 +0800

    在dao类和service类编写接口及其实现方法使数据库表中的信息显示在前端页面

--- src/main/java/com/lzjtugrp2/service/impl/TaskCycleServiceImpl.java
+++ src/main/java/com/lzjtugrp2/service/impl/TaskCycleServiceImpl.java
@@ -7,12 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.util.List;
 @Service
 @Transactional
-
 public class TaskCycleServiceImpl implements TaskCycleService {
-    @Autowired
+
+    @Resource
     private TaskCycleMapper taskcycleMapper;
 
     @Override
