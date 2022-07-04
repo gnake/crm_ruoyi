21a5f1a3b61e9e705675689da03b906ad786b34b
泽楷 李 <1260644546@qq.com>
2022-07-04 23:50:40 +0800

    创建登录功能的service类

---
+++ src/main/java/com/lzjtugrp2/service/CustomerLoginService.java
@@ -0,0 +1,7 @@
+package com.lzjtugrp2.service;
+
+import org.springframework.stereotype.Service;
+
+public interface CustomerLoginService {
+    boolean findUserByDetail(String usercode,String password) throws Exception;
+}
