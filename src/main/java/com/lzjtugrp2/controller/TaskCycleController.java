fab06852179364726784bb44a4608d2940cff314
swj <2575901953@qq.com>
2022-07-04 18:08:20 +0800

    修改了SQL查询

--- src/main/java/com/lzjtugrp2/controller/TaskCycleController.java
+++ src/main/java/com/lzjtugrp2/controller/TaskCycleController.java
@@ -2,49 +2,46 @@ package com.lzjtugrp2.controller;
 
 import com.lzjtugrp2.domain.TaskCycle;
 import com.lzjtugrp2.service.TaskCycleService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import javax.annotation.Resource;
 import java.util.List;
 
 @Controller
-@RequestMapping("task")
 public class TaskCycleController {
-    @Autowired
+    @Resource
     private TaskCycleService taskcycleService;
 
     @GetMapping("/taskCycle")
     public String taskcycle(Model model){
         List<TaskCycle> taskList = taskcycleService.getAllTask();
         model.addAttribute("taskList", taskList);
-        return "taskCycle";
+        return "taskcycle";
     }
     //新增
     @GetMapping("/taskcycleadd")
     public String insert(){
-        return "taskCycle";
+        return "taskcycle";
     }
     //编辑
     @GetMapping("/update")
     public String update(){
-        return "taskCycle";
+        return "taskcycle";
     }
     //复制
     @GetMapping("/copy")
     public String copy(){
-        return "taskCycle";
+        return "taskcycle";
     }
     //查看
     @GetMapping("/select")
     public String select(){
-        return "taskCycle";
+        return "taskcycle";
     }
     //删除
     @GetMapping("/delete")
     public String delete(){
-        return "taskCycle";
+        return "taskcycle";
     }
 }
