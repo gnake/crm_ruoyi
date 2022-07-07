ff42414a53d9a1c9b0c6c3c897b6bde40d7dea26
泽楷 李 <1260644546@qq.com>
2022-07-07 18:34:15 +0800

    客户管理的service类

---
+++ ruoyi-admin/src/main/java/com/ruoyi/crm/service/ICustomerInfoService.java
@@ -0,0 +1,61 @@
+package com.ruoyi.crm.service;
+
+import java.util.List;
+import com.ruoyi.crm.domain.CustomerInfo;
+
+/**
+ * 客户信息Service接口
+ * 
+ * @author 李泽楷
+ * @date 2022-07-07
+ */
+public interface ICustomerInfoService 
+{
+    /**
+     * 查询客户信息
+     * 
+     * @param custId 客户信息主键
+     * @return 客户信息
+     */
+        CustomerInfo selectCustomerInfoByCustId(Long custId);
+
+    /**
+     * 查询客户信息列表
+     * 
+     * @param customerInfo 客户信息
+     * @return 客户信息集合
+     */
+    List<CustomerInfo> selectCustomerInfoList(CustomerInfo customerInfo);
+
+    /**
+     * 新增客户信息
+     * 
+     * @param customerInfo 客户信息
+     * @return 结果
+     */
+    int insertCustomerInfo(CustomerInfo customerInfo);
+
+    /**
+     * 修改客户信息
+     * 
+     * @param customerInfo 客户信息
+     * @return 结果
+     */
+    int updateCustomerInfo(CustomerInfo customerInfo);
+
+    /**
+     * 批量删除客户信息
+     * 
+     * @param custIds 需要删除的客户信息主键集合
+     * @return 结果
+     */
+    int deleteCustomerInfoByCustIds(String custIds);
+
+    /**
+     * 删除客户信息信息
+     * 
+     * @param custId 客户信息主键
+     * @return 结果
+     */
+    int deleteCustomerInfoByCustId(Long custId);
+}
